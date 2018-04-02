061a4cea6bff40fd9a819ad2008d5dbc1c308080
Fan Zhang <fzhang.combio@gmail.com>
2018-04-02 13:28:19 -0400

    update facet_grid

--- R/barplot_all_fine_clusters_per_patient.R
+++ R/barplot_all_fine_clusters_per_patient.R
@@ -5,12 +5,14 @@
 #' Figure 2
 #' ---
 
-setwd("/Users/fanzhang/Documents/GitHub/amp_phase1_ra/")
+setwd("/Users/fanzhang/Documents/GitHub/amp_phase1_ra/R/")
 
-source("R/meta_colors.R")
+source("meta_colors.R")
+library(gdata) 
+library(ggplot2)
 
 # Read post-QC single-cell RNA-seq meta data
-meta <- readRDS("data/celseq_synovium_meta.rds")
+meta <- readRDS("../data/celseq_synovium_meta.rds")
 meta <- meta[order(meta$cell_name),]
 meta$cell_name <- as.character(meta$cell_name)
 dim(meta)
@@ -24,20 +26,19 @@ dim(meta)
 
 all(rownames(x) == meta$cell_name)
 meta$fine_cluster <- x$fine_cluster
-mat <- meta[, c("sample", "disease", "plate", "fine_cluster")]
+mat <- meta[, c("sample", "disease", "plate", "fine_cluster", "type")]
 non_samples <- names(which(table(mat$sample) == 0))
 
-dat <- table(mat$fine_cluster, mat$sample)
+dat <- table(mat$fine_cluster, mat$sample) 
 dat <- as.data.frame(dat)
 colnames(dat) <- c("fine_cluster", "sample", "freq")
 dat <- dat[-which(dat$sample %in% non_samples),]
 dat$sample <- as.character(dat$sample)
-
 table(dat$sample)
 dim(dat)
 
 # Read Case.Control labels for OA, non-inflamed RA, and RA
-inflam_label <- read.xls("data-raw/postQC_all_samples.xlsx")
+inflam_label <- read.xls("../data-raw/postQC_all_samples.xlsx")
 inflam_label <- inflam_label[c(1:51),]
 table(inflam_label$Case.Control)
 inflam_label <- inflam_label[which(inflam_label$Patient %in% dat$sample), ]
@@ -45,11 +46,20 @@ inflam_label <- inflam_label[, c("Patient", "Disease", "Tissue.type", "Case.Cont
 colnames(inflam_label)[1] <- "sample"
 plot_final <- merge(dat, inflam_label, by = "sample")
 dim(plot_final)
+plot_final[1:4,]
+
+# Add cell type column
+type <- rep("Fibroblast", nrow(plot_final))
+type[grep("B-", plot_final$fine_cluster)] <- "B cell"
+type[grep("T-", plot_final$fine_cluster)] <- "T cell"
+type[grep("M-", plot_final$fine_cluster)] <- "Monocyte"
+plot_final$type <- type
 
 plot_final$Case_Control_order = factor(plot_final$Case.Control, levels=c('OA','non-inflamed RA','inflamed RA'))
+plot_final$type_order = factor(plot_final$type, levels=c('Fibroblast','T cell','B cell', 'Monocyte'))
 
 # saveRDS(plot_final, "barplot_sc_cluster_per_patient.rds")
-plot_final <- readRDS("data/barplot_sc_cluster_per_patient.rds")
+plot_final <- readRDS("../data/barplot_sc_cluster_per_patient.rds")
   
 # Plot cells from all single-cell RNA-seq clusters for each patient
 ggplot(
@@ -60,22 +70,21 @@ ggplot(
            # position = "fill"
            position = "stack"
            ) +
-  facet_grid(~ Case_Control_order, scales = "free", space = "free_x") +
+  facet_grid(type_order ~ Case_Control_order, scales = "free", space = "free_x") +
   scale_fill_manual("", values = meta_colors$fine_cluster) +
   labs(
     x = "Donors",
     # y = "Proportion of all clusters"
     y = "Number of cells"
   ) +
-  theme_bw(base_size = 25) +
+  theme_bw(base_size = 25.5) +
   theme(    
         # axis.ticks = element_blank(), 
         panel.grid = element_blank(),
         axis.text = element_text(size = 25),
         axis.text.x=element_text(angle=35, hjust=1),
-        axis.text.y = element_text(size = 25))
+        axis.text.y = element_text(size = 35))
 ggsave(file = paste("barplot_sc_cluster_per_patient", ".pdf", sep = ""),
        width = 15, height = 12, dpi = 300)
 dev.off()
-
 
